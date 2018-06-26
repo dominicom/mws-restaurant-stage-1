8c74624c18d37042b61300b6ba62965576649d45
dominicom <dominik.kiepuszewski@gmail.com>
2018-06-26 19:38:30 +0200

    Added ServiceWorker and fixed styles on main page

--- js/main.js
+++ js/main.js
@@ -9,7 +9,19 @@ var markers = []
  */
 document.addEventListener('DOMContentLoaded', (event) => {
 
-  // ServiceWorker tutaj
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/serviceworker/sw.js')
+      .then( (registration) => {
+        // Registration was successful
+        console.log('ServiceWorker registered, scope: ', registration.scope);
+      })
+      .catch( (error) => {
+        // registration failed
+        console.log('ServiceWorker registration failed: ', error);
+      });
+
+  }
 
 
   initMap(); // added
@@ -79,7 +91,7 @@ initMap = () => {
   self.newMap = L.map('map', {
         center: [40.722216, -73.987501],
         zoom: 12,
-        scrollWheelZoom: true  //false
+        scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
     mapboxToken: 'pk.eyJ1IjoiZG9taW5pY29tIiwiYSI6ImNqaWJ1djgxZjFtMXMzcGxndjVtY2kwNTcifQ.mSBj4uB0ilknv9tWABt8fQ',
@@ -92,18 +104,6 @@ initMap = () => {
 
   updateRestaurants();
 }
-/* window.initMap = () => {
-  let loc = {
-    lat: 40.722216,
-    lng: -73.987501
-  };
-  self.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: loc,
-    scrollwheel: false
-  });
-  updateRestaurants();
-} */
 
 /**
  * Update page and map for current restaurants.
@@ -160,30 +160,70 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
+  const link = document.createElement('a');
+  // link.innerHTML = 'View Details';
+  link.href = DBHelper.urlForRestaurant(restaurant);
+  link.className = restaurant.cuisine_type.toLowerCase();
+  li.append(link);
+
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  link.append(image);
+
+  const label = document.createElement('div');
+  label.className = 'restaurant-label';
+  link.append(label);
 
   const name = document.createElement('h1');
+  name.className = 'restaurant-name';
   name.innerHTML = restaurant.name;
-  li.append(name);
+  label.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  label.append(neighborhood);
 
   const address = document.createElement('p');
+  address.className = 'restaurant-address';
   address.innerHTML = restaurant.address;
-  li.append(address);
+  label.append(address);
 
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  const hr = document.createElement('hr');
+  label.append(hr);
+
+  const rating = document.createElement('span');
+  rating.className = 'rating';
+  rating.innerHTML = 'Rating: ' + restaurantRating(restaurant); //'rating';
+  label.append(rating);
+
+  const type = document.createElement('span');
+  type.className = 'cuisine-type';
+  type.innerHTML = restaurant.cuisine_type;
+  label.append(type);
+
+
+
+
+
+
+  // const more = document.createElement('a');
+  // more.innerHTML = 'View Details';
+  // more.href = DBHelper.urlForRestaurant(restaurant);
+  // li.append(more)
 
   return li
 }
+
+restaurantRating = (restaurant) => {
+  let reviews = restaurant.reviews.map( (r) => r.rating);
+  let rating = reviews.reduce((a, b) => a + b, 0) / reviews.length;
+  rating = rating.toFixed(1);
+
+  return rating;
+}
+
+
 
 /**
  * Add markers for current restaurants to the map.
@@ -198,13 +238,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     }
   });
 }
-/* addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
