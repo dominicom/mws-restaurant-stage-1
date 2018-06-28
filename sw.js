abdf43220a058df57a2e8681b44b34e3965feb8c
dominicom <dominik.kiepuszewski@gmail.com>
2018-06-28 19:01:11 +0200

    fixed path in SW

--- sw.js
+++ sw.js
@@ -18,16 +18,16 @@ const assets = [
   '/js/dbhelper.js',
   '/js/restaurant_info.js',
   '/data/restaurants.json',
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/6.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg'
+  '/restaurant-reviews-app/img/1.jpg',
+  '/restaurant-reviews-app/img/2.jpg',
+  '/restaurant-reviews-app/img/3.jpg',
+  '/restaurant-reviews-app/img/4.jpg',
+  '/restaurant-reviews-app/img/5.jpg',
+  '/restaurant-reviews-app/img/6.jpg',
+  '/restaurant-reviews-app/img/7.jpg',
+  '/restaurant-reviews-app/img/8.jpg',
+  '/restaurant-reviews-app/img/9.jpg',
+  '/restaurant-reviews-app/img/10.jpg'
 ];
 
 self.addEventListener('install', function(event) {
